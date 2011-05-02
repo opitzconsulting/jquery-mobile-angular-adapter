e4f288d1a2be0a032b89feac6796cad89c653823
Tobias Bosch <tobias.bosch@opitz-consulting.com>
2011-05-02 14:13:27 +0200

    bugfix of ngm:click no more preventing clicks

--- src/jquery-mobile-angular-adapter.js
+++ src/jquery-mobile-angular-adapter.js
@@ -33,47 +33,48 @@
  * Basic compile integration.
  */
 (function(angular, $) {
-	var globalScope = null;
-	
-	/**
-	 * Lazily initializes the global scope. If a controller named
-	 * MainController exists, it will be used as the controller
-	 * for the global scope.  The global scope can be used
-	 * to communicate between the pages.
-	 */
-	function getGlobalScope() {
-		if (globalScope) {
-			return globalScope;
-		}
-		// Always use a singleton controller for that main scope.
-		// create a global scope over all pages,
-		// so common data is possible. 
-		globalScope = angular.scope();
-		if (window.MainController) {
-			globalScope.$become(MainController);
-		}
-		return globalScope;
-	}
+    var globalScope = null;
+
+    /**
+     * Lazily initializes the global scope. If a controller named
+     * MainController exists, it will be used as the controller
+     * for the global scope.  The global scope can be used
+     * to communicate between the pages.
+     */
+    function getGlobalScope() {
+        if (globalScope) {
+            return globalScope;
+        }
+        // Always use a singleton controller for that main scope.
+        // create a global scope over all pages,
+        // so common data is possible.
+        globalScope = angular.scope();
+        if (window.MainController) {
+            globalScope.$become(MainController);
+        }
+        return globalScope;
+    }
 
     $.mobile.globalScope = function() {
-        if (arguments.length==0) {
+        if (arguments.length == 0) {
             return getGlobalScope();
         } else {
             globalScope = arguments[0];
         }
     };
 
-	// saves the fact whether the original page()
+    // saves the fact whether the original page()
     // method is currently enhancing the page.
     $.mobile.inPagePreCompile = false;
 
-	/*
-	 * create an own scope for every page when the page is initialized.
-	 */
+    /*
+     * create an own scope for every page when the page is initialized.
+     */
     var recursivePageCall = false;
     var oldPage = $.fn.page;
-	$.fn.page = function() {
+    $.fn.page = function() {
         var self = this;
+
         function callOrigPage() {
             var oldPageCompile = $.mobile.inPagePreCompile;
             $.mobile.inPagePreCompile = true;
@@ -81,34 +82,35 @@
             $.mobile.inPagePreCompile = oldPageCompile;
             return res;
         }
+
         var oldRecursivePageCall = recursivePageCall;
         recursivePageCall = true;
         var instanceExists = this.data() && this.data().page;
         var res = callOrigPage();
-		if (!instanceExists && !oldRecursivePageCall) {
+        if (!instanceExists && !oldRecursivePageCall) {
             // sometime widgets like the selectmenu create
             // dynamic pages (e.g. for selecting values, ...).
             // Those pages only contain generic markup,
             // nothing that angular needs to enhance!
             var childScope = getGlobalScope().$new();
             angular.compile(this)(childScope);
-		}
+        }
         recursivePageCall = oldRecursivePageCall;
 
         return res;
-	};
+    };
 
 
-	// listen to pageshow and update the angular $location-service.
-	// Prevents an errornous back navigation when navigating to another page.
-	// This occurs when angular does things by an xhr and it's eval
-	// method takes some time to run (race condition...).
-	// See $location service, especially the sync and updateBrowser functions.
-	angular.service("createWatchPageShow", function($browser, $location) {
-		$('.ui-page').live('pageshow', function(event, ui){
-			$location.update($browser.getUrl());
-		});
-	}, {$inject: ['$browser','$location'], $eager:true});
+    // listen to pageshow and update the angular $location-service.
+    // Prevents an errornous back navigation when navigating to another page.
+    // This occurs when angular does things by an xhr and it's eval
+    // method takes some time to run (race condition...).
+    // See $location service, especially the sync and updateBrowser functions.
+    angular.service("createWatchPageShow", function($browser, $location) {
+        $('.ui-page').live('pageshow', function(event, ui) {
+            $location.update($browser.getUrl());
+        });
+    }, {$inject: ['$browser','$location'], $eager:true});
 })(angular, jQuery);
 
 
@@ -116,22 +118,26 @@
  * Integration of jquery mobile and angular widgets.
  */
 (function(angular) {
-	/* A widget for clicks.
-	 * Just as ng:click. However, also prevents the default action.
-	 */
-	angular.directive("ngm:click", function(expression, element){
-		var linkFn = function($updateView, element){
-		    var self = this;
-		    element.bind('vclick', function(event){
-		      var res = self.$tryEval(expression, element);
-		      $updateView();
-              event.stopPropagation();
-              event.preventDefault();
-		    });
-		  };
-		  linkFn.$inject = ['$updateView'];
-		  return linkFn;
-		});
+    /* A widget for clicks.
+     * Just as ng:click. However, also prevents the default action.
+     */
+    angular.directive("ngm:click", function(expression, element) {
+        var linkFn = function($updateView, element) {
+            var self = this;
+            element.bind('vclick', function(event) {
+                var res = self.$tryEval(expression, element);
+                $updateView();
+                event.stopPropagation();
+                event.preventDefault();
+                if (event.originalEvent) {
+                    event.originalEvent.stopPropagation();
+                    event.originalEvent.preventDefault();
+                }
+            });
+        };
+        linkFn.$inject = ['$updateView'];
+        return linkFn;
+    });
 
     /*
      * Integration of the slider and selectmenu widget of jquery mobile:
@@ -143,7 +149,7 @@
     $.fn.origSlider = $.fn.slider;
     $.fn.slider = function() {
         var instanceExists = this.data() && this.data().slider;
-        if ($.mobile.inPagePreCompile && arguments.length==0 && !instanceExists) {
+        if ($.mobile.inPagePreCompile && arguments.length == 0 && !instanceExists) {
             // Prevent initialization during precompile,
             // and mark the element so that the angular widget
             // can create the widget!
@@ -157,7 +163,7 @@
     $.fn.origSelectmenu = $.fn.selectmenu;
     $.fn.selectmenu = function() {
         var instanceExists = this.data() && this.data().selectmenu;
-        if ($.mobile.inPagePreCompile && arguments.length==0 && !instanceExists) {
+        if ($.mobile.inPagePreCompile && arguments.length == 0 && !instanceExists) {
             // Prevent initialization during precompile,
             // and mark the element so that the angular widget
             // can create the widget!
@@ -169,13 +175,14 @@
     }
 
     var oldSelect = angular.widget('select');
-    angular.widget('select', function(element){
+    angular.widget('select', function(element) {
         var name = element.attr('name');
         var disabled = element.attr('disabled');
         var oldRes = oldSelect.apply(this, arguments);
         var myRes = function($updateView, $defer, element) {
             var mwidget = element.attr('mwidget');
             var created = false;
+
             function updateEnabled(element) {
                 var disabled = element.attr('disabled');
                 if (disabled) {
@@ -184,6 +191,7 @@
                     element[mwidget]('enable');
                 }
             }
+
             // The current element may not be inserted into the dom correctly yet
             // (e.g. due to ng:repeat). However, some jquery mobile
             // widgets like selectmenu
@@ -208,9 +216,9 @@
             // Detect changes in the disabled attribute.
             // Needs to be done last in the eval cycle,
             // as angular sets this attribute.
-            scope.$onEval(Number.MAX_VALUE, function(){
+            scope.$onEval(Number.MAX_VALUE, function() {
                 var val = element.attr('disabled');
-                if (val!=oldVal) {
+                if (val != oldVal) {
                     oldVal = val;
                     if (created) {
                         updateEnabled(element);
@@ -231,7 +239,7 @@
  * onactiveate and onpassivate callbacks for scopes
  */
 (function(angular, $) {
-    $('div').live('pagebeforehide',function(event, ui){
+    $('div').live('pagebeforehide', function(event, ui) {
         var currPageScope = $(event.target).scope();
         var nextPage = ui.nextPage;
         var nextPageScope = nextPage && nextPage.scope();
@@ -240,7 +248,7 @@
         }
     });
 
-    $('div').live('pagebeforeshow',function(event, ui){
+    $('div').live('pagebeforeshow', function(event, ui) {
         var currPageScope = $(event.target).scope();
         var prevPage = ui.prevPage;
         var prevPageScope = prevPage && prevPage.scope();
@@ -253,7 +261,7 @@
 /*
  * Special angular services for jquery mobile
  */
-(function(angular,window) {
+(function(angular, window) {
     /*
      * Service for page navigation.
      * A call without parameters returns the current page id.
@@ -264,47 +272,48 @@
      */
     angular.service('$activePage', function() {
         return function() {
-            if (arguments.length==0) {
-               var currPage = $.mobile.activePage;
+            if (arguments.length == 0) {
+                var currPage = $.mobile.activePage;
                 if (currPage) {
                     return currPage.attr('id');
                 } else {
                     return null;
                 }
-           } else {
-               // set the page...
-               var pageId = arguments[0];
-               if (pageId=='back') {
-                   window.history.back();
-               } else {
-                   $.mobile.changePage.apply($.mobile.changePage, arguments);
-               }
-           }
+            } else {
+                // set the page...
+                var pageId = arguments[0];
+                if (pageId == 'back') {
+                    window.history.back();
+                } else {
+                    $.mobile.changePage.apply($.mobile.changePage, arguments);
+                }
+            }
         };
     });
 
-})(angular,window);
+})(angular, window);
 
 
 /*
  * Defines templating mechanisms useful for jquery mobile
  */
 (function(angular) {
-	var templates = {};
+    var templates = {};
 
-	function quickClone(element) {
-		  return angular.element(element[0].cloneNode(true));
-	}
+    function quickClone(element) {
+        return angular.element(element[0].cloneNode(true));
+    }
 
-	function eachAttribute(element, fn){
-		  var i, attrs = element[0].attributes || [], attr, name, value = {};
-		  for (i = 0; i < attrs.length; i++) {
-		    attr = attrs[i];
-		    name = attr.name;
-		    value = attr.value;
-		    fn(name, value);
-		  }
-		}
+    function eachAttribute(element, fn) {
+        var i, attrs = element[0].attributes || [], attr, name, value = {};
+        for (i = 0; i < attrs.length; i++) {
+            attr = attrs[i];
+            name = attr.name;
+            value = attr.value;
+            fn(name, value);
+        }
+    }
+
     /*
      * Directive that fades in an element when angular
      * uses it. Useful in templating when the underlying template changed.
@@ -318,65 +327,66 @@
         };
     });
 
-	/**
-	 * Defines a template to be used later by {@ngm:switch}. The value of the
-	 * attribute is the template id.
-	 */
-	angular.widget("@ngm:define", function(expression, element) {
-		element.removeAttr('ngm:define');
+    /**
+     * Defines a template to be used later by {@ngm:switch}. The value of the
+     * attribute is the template id.
+     */
+    angular.widget("@ngm:define", function(expression, element) {
+        element.removeAttr('ngm:define');
         templates[expression] = element;
-		// hide the element, but do not remove it from the dom,
-		// as otherwise the iteration in angular over the dom
-		// gets confused!
-		element.hide();
-		// do not allow child tags nor directives, as we want to capture them!
-		this.directives(false);
-		this.descend(false);
-		// and do nothing in the linkage-phase
-		return function() {
+        // hide the element, but do not remove it from the dom,
+        // as otherwise the iteration in angular over the dom
+        // gets confused!
+        element.hide();
+        // do not allow child tags nor directives, as we want to capture them!
+        this.directives(false);
+        this.descend(false);
+        // and do nothing in the linkage-phase
+        return function() {
 
-		};
-	});
+        };
+    });
 
-	/**
-	 * Applies a template. The value of this attribute needs to be an angular expression
-	 * that evaluates to a template id defined by {@ngm:define}. When the expression
-	 * changes, the template also changes.
-	 */
-	angular.widget("@ngm:switch", function(expression, element) {
-		var compiler = this;
-		element.removeAttr('ngm:switch');
-		return function(element) {
-			var scope = this;
+    /**
+     * Applies a template. The value of this attribute needs to be an angular expression
+     * that evaluates to a template id defined by {@ngm:define}. When the expression
+     * changes, the template also changes.
+     */
+    angular.widget("@ngm:switch", function(expression, element) {
+        var compiler = this;
+        element.removeAttr('ngm:switch');
+        return function(element) {
+            var scope = this;
 
-			scope.$watch(expression, function(tplId) {
-				var templateEntry = templates[tplId];
-				if (!templateEntry) {
-					element.hide();
-					return;
-				};
-				var newElement = quickClone(templateEntry);
-				newElement.show();
-				// remove all children
-				element.html('');
-				eachAttribute(element, function(name, value) {
-					element.removeAttr(name);
-				});
-				// add the attributes of the template
-				eachAttribute(newElement, function(name, value) {
-					element.attr(name, value);
-				});
-				// and also all children of the template
-				element.append(newElement.contents());
-				// now reevaluate the element again.
-				// Attention: keep the old jquery element in the scope correct!
+            scope.$watch(expression, function(tplId) {
+                var templateEntry = templates[tplId];
+                if (!templateEntry) {
+                    element.hide();
+                    return;
+                }
+                ;
+                var newElement = quickClone(templateEntry);
+                newElement.show();
+                // remove all children
+                element.html('');
+                eachAttribute(element, function(name, value) {
+                    element.removeAttr(name);
+                });
+                // add the attributes of the template
+                eachAttribute(newElement, function(name, value) {
+                    element.attr(name, value);
+                });
+                // and also all children of the template
+                element.append(newElement.contents());
+                // now reevaluate the element again.
+                // Attention: keep the old jquery element in the scope correct!
                 var oldScopeElement = scope.$element;
                 angular.compile(element)(scope);
                 scope.$element = oldScopeElement;
-			});
+            });
 
-		};
-	});
+        };
+    });
 })(angular);
 
 

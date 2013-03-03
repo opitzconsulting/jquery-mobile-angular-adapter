59a1e96b947775bb70eb794e09bdfe1bfc397949
Tobias Bosch <tobias.bosch@opitz-consulting.com>
2013-03-03 18:01:40 +0100

    changed travis config to chrome due to bugs in firefox with iframes and pushstate

--- .travis/scripts/install_chrome.sh
+++ .travis/scripts/install_chrome.sh
@@ -4,5 +4,7 @@ BASEDIR=$(dirname $0)
 BASEDIR=$(readlink -f "$BASEDIR/..")
 
 sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
-sudo apt-get update
-sh -e $BASEDIR/scripts/apt-get.sh "xvfb imagemagick google-chrome-stable"
+
+sudo apt-get update --force-yes
+# sudo apt-get install -qq --force-yes xvfb imagemagick google-chrome-unstable
+sudo apt-get install -qq --force-yes xvfb imagemagick google-chrome-stable
